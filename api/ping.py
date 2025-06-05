ae954c37a68b1cb332ad69db9793e52ced521f2f
gerhard le Roux <gerhardlr@gmail.com>
2025-06-05 13:21:33 +0000

    check

--- api/ping.py
+++ api/ping.py
@@ -2,11 +2,10 @@ import json
 from typing import Dict, Any
 
 
-from _types import VercelRequest
 from .redis_db import ping_db
 
 
-def handler(request: VercelRequest) -> Dict[str, Any]:
+def handler(request: Any) -> Dict[str, Any]:
 
     timestamp = ping_db()
     return {
