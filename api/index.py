1e91e2808155b54e6103d11225b7fb2378570341
Gerhard le Roux <gerhardlr@gmail.com>
2025-08-12 18:26:47 +0000

    change path

--- api/index.py
+++ api/index.py
@@ -33,7 +33,7 @@ def get_event():
     return {"warning": f'DB is not available'}
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/metrics", response_class=HTMLResponse)
 def get_event_root(request: Request):
     db = get_db()
     if result := db.ping():
